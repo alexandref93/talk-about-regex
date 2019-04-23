93bc82f1926745734da0fcd52697d497ed33466e
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2019-04-22 23:41:56 -0300

    feat(core): add more content

---
+++ theme.js
@@ -0,0 +1,10 @@
+export default {
+    // add a custom font
+    font: 'Roboto, sans-serif',
+    // custom colors
+    colors: {
+      text: '#f0f',
+      background: 'black',
+      link: '#0ff',
+    },
+  }
